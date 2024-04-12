20c2c1bf6fead4f8838e9ba585c6eefe401e0e7c
eberegit <samsoneberenjoku@gmail.com>
2024-04-12 10:23:14 +0100

    updates

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 import { config } from "dotenv";
 import { ChatOpenAI } from "@langchain/openai";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
 
 config();
 
 const chatModel = new ChatOpenAI({});
 const outputParser = new StringOutputParser();
 
-const prompt = ChatPromptTemplate.fromMessages([
-  [
-    "system",
-    "You are a world class documentation assistant only. Do not answer any other questions asides on documentation",
-  ],
+// const prompt = ChatPromptTemplate.fromMessages([
+//   [
+//     "system",
+//     "You are a world class documentation assistant only. Do not answer any other questions asides on documentation",
+//   ],
+//   ["user", "{input}"],
+// ]);
+
+const historyAwarePrompt = ChatPromptTemplate.fromMessages([
+  new MessagesPlaceholder("chat_history"),
   ["user", "{input}"],
+  [
+    "user",
+    "Given the above conversation, provide a befitting feedback",
+  ],
 ]);
 
-const chain = prompt.pipe(chatModel).pipe(outputParser);
+const chatHistory = [
+  new HumanMessage("Can you write a drama with plot and characters?"),
+  new AIMessage("Yes!"),
+];
+
+const chain = historyAwarePrompt.pipe(chatModel).pipe(outputParser);
 
 const result = await chain.invoke({
-  input: "Hello",
+  chat_history: chatHistory,
+  input: "Convince me with a sample",
 });
 
 console.log(result);
